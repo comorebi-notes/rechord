75b89200bc789c98eef787ee0732160f0bd2aec2
kero-uzura <wakabayashita@firstserver.co.jp>
2017-11-01 18:32:36 +0900

    add sample page for webpacker testing

--- Gemfile
+++ Gemfile
@@ -34,6 +34,9 @@ gem 'jbuilder', '~> 2.5'
 # gem 'capistrano-rails', group: :development
 
 gem 'webpacker', '~> 3.0'
+gem 'font-awesome-rails'
+gem 'rails-i18n'
+gem 'slim-rails'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -51,4 +51,7 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  # Make javascript_pack_tag load assets from webpack-dev-server.
+  config.x.webpacker[:dev_server_host] = "http://localhost:8080"
 end

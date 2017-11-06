697a7189cc4e1d146b8623a825d193858345452c
kero-uzura <biruge@biruge.com>
2017-11-06 22:41:11 +0900

    add translation

--- app/frontend/utils/translate.js
+++ app/frontend/utils/translate.js
@@ -2,9 +2,10 @@
 
 const translate = (chordType) => {
   if (chordType === "add9")   return "Madd9"
+  if (chordType === "mM7")    return "mMaj7"
+  if (chordType === "m7-5")   return "m7b5"
   if (chordType === "dim")    return "o"
   if (chordType === "dim7")   return "o7"
-  if (chordType === "m7-5")   return "m7b5"
   if (chordType === "+5")     return "M#5"
   if (chordType === "m7(11)") return "m7add11"
   if (chordType.match(/\(.+\)/)) {

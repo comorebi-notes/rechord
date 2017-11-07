8d2e594b6a96810b520178587bf76fd654ef9341
kero-uzura <wakabayashita@firstserver.co.jp>
2017-11-07 13:41:41 +0900

    refactor

--- app/frontend/components/shared/button.js
+++ app/frontend/components/shared/button.js
@@ -4,15 +4,17 @@ import classNames               from "classnames"
 export default class Button extends PureComponent {
   render() {
     const { color, size, icon, text, disabled, onClick } = this.props
+    const buttonClass = classNames("button", { [`is-${color}`]: color, [`is-${size}`]: size })
+    const iconClass = classNames("fa", { [`fa-${icon}`]: icon })
     return (
       <button
         onClick={onClick}
-        className={classNames("button", { [`is-${color}`]: color }, { [`is-${size}`]: size })}
+        className={buttonClass}
         disabled={disabled}
       >
         {icon && (
           <span className="icon is-small">
-            <i className={classNames("fa", { [`fa-${icon}`]: icon })} />
+            <i className={iconClass} />
           </span>
         )}
         <span>{text}</span>

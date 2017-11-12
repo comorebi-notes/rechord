4148f916f6443ea83a0a3017fa8780e2b759b710
kero-uzura <biruge@biruge.com>
2017-11-13 01:12:42 +0900

    add title and comment (wip)

--- app/frontend/components/App/index.js
+++ app/frontend/components/App/index.js
@@ -81,146 +81,165 @@ export default class App extends Component {
     const placeholder = ["# e.g.", "D6(9) | Aadd9 | E | F#m7(11)"].join("\n")
     return (
       <div>
-        <div className="columns">
-          <div className="column control">
-            <ScoreEditor
-              inputText={inputText}
-              editorState={editorState}
-              placeholder={placeholder}
-              handleChangeEditorState={this.handleChangeEditorState}
-              readOnly={isPlaying}
-            />
+        <div className="field">
+          <label className="label">Title</label>
+          <div className="control">
+            <input className="input" type="text" placeholder="title" style={{ width: "50%" }} />
           </div>
+        </div>
 
-          <div className="column control-ui">
+        <div className="field">
+          <label className="label">Comment</label>
+          <div className="control">
+            <input className="input" type="text" placeholder="comment" />
+          </div>
+        </div>
+
+        <div className="field">
+          <label className="label">Score</label>
+          <div className="control">
             <div className="columns">
-              <div className="column editor-control">
-                <HasAddonsField customClass="key-control">
-                  <div className="control">
-                    <Button
-                      onClick={this.handleUndo}
-                      icon="undo"
-                      disabled={isPlaying || editorState.getUndoStack().size === 0}
-                    />
-                  </div>
-                  <div className="control">
-                    <Button
-                      onClick={this.handleRedo}
-                      icon="repeat"
-                      disabled={isPlaying || editorState.getRedoStack().size === 0}
-                    />
-                  </div>
-                </HasAddonsField>
-                <HasAddonsField customClass="key-control">
-                  <div className="control">
-                    <Button
-                      onClick={() => this.handleKeyChange("up")}
-                      text="#"
-                      disabled={isPlaying}
-                    />
-                  </div>
-                  <div className="control">
-                    <Button
-                      onClick={() => this.handleKeyChange("down")}
-                      text="b"
-                      disabled={isPlaying}
-                    />
-                  </div>
-                </HasAddonsField>
-                <Field>
-                  <Button
-                    onClick={this.handleClearText}
-                    icon="trash"
-                    text="clear"
-                    disabled={isPlaying}
-                  />
-                </Field>
-                <Field>
-                  <Button
-                    onClick={this.handleSetSample}
-                    icon="tasks"
-                    text="sample"
-                    disabled={isPlaying}
-                  />
-                </Field>
+              <div className="column control">
+                <ScoreEditor
+                  inputText={inputText}
+                  editorState={editorState}
+                  placeholder={placeholder}
+                  handleChangeEditorState={this.handleChangeEditorState}
+                  readOnly={isPlaying}
+                />
               </div>
 
-              <div className="column">
-                <SelectField icon="music" customClass="instrument-control">
-                  <select
-                    value={instrument}
-                    onChange={this.handleChangeInstrument}
-                    disabled={isPlaying}
-                  >
-                    {Object.keys(instruments.types()).map(type => (
-                      <option key={type} value={type}>
-                        {type}
-                      </option>
-                    ))}
-                  </select>
-                </SelectField>
-                <HorizontalField label="Time">
-                  <SelectField customClass="time-control">
-                    <select
-                      value={time}
-                      onChange={this.handleChangeTime}
-                      disabled={isPlaying}
-                    >
-                      {Object.keys(times).map(timeKey => (
-                        <option key={timeKey} value={timeKey}>
-                          {timeKey}
-                        </option>
-                      ))}
-                    </select>
-                  </SelectField>
-                </HorizontalField>
-                <HorizontalField label="BPM">
-                  <input
-                    type="number"
-                    min={MIN_BPM}
-                    max={MAX_BPM}
-                    className="input"
-                    value={bpm}
-                    onChange={this.handleChangeBpm}
+              <div className="column control-ui">
+                <div className="columns">
+                  <div className="column editor-control">
+                    <HasAddonsField customClass="key-control">
+                      <div className="control">
+                        <Button
+                          onClick={this.handleUndo}
+                          icon="undo"
+                          disabled={isPlaying || editorState.getUndoStack().size === 0}
+                        />
+                      </div>
+                      <div className="control">
+                        <Button
+                          onClick={this.handleRedo}
+                          icon="repeat"
+                          disabled={isPlaying || editorState.getRedoStack().size === 0}
+                        />
+                      </div>
+                    </HasAddonsField>
+                    <HasAddonsField customClass="key-control">
+                      <div className="control">
+                        <Button
+                          onClick={() => this.handleKeyChange("up")}
+                          text="#"
+                          disabled={isPlaying}
+                        />
+                      </div>
+                      <div className="control">
+                        <Button
+                          onClick={() => this.handleKeyChange("down")}
+                          text="b"
+                          disabled={isPlaying}
+                        />
+                      </div>
+                    </HasAddonsField>
+                    <Field>
+                      <Button
+                        onClick={this.handleClearText}
+                        icon="trash"
+                        text="clear"
+                        disabled={isPlaying}
+                      />
+                    </Field>
+                    <Field>
+                      <Button
+                        onClick={this.handleSetSample}
+                        icon="tasks"
+                        text="sample"
+                        disabled={isPlaying}
+                      />
+                    </Field>
+                  </div>
+
+                  <div className="column">
+                    <SelectField icon="music" customClass="instrument-control">
+                      <select
+                        value={instrument}
+                        onChange={this.handleChangeInstrument}
+                        disabled={isPlaying}
+                      >
+                        {Object.keys(instruments.types()).map(type => (
+                          <option key={type} value={type}>
+                            {type}
+                          </option>
+                        ))}
+                      </select>
+                    </SelectField>
+                    <HorizontalField label="Time">
+                      <SelectField customClass="time-control">
+                        <select
+                          value={time}
+                          onChange={this.handleChangeTime}
+                          disabled={isPlaying}
+                        >
+                          {Object.keys(times).map(timeKey => (
+                            <option key={timeKey} value={timeKey}>
+                              {timeKey}
+                            </option>
+                          ))}
+                        </select>
+                      </SelectField>
+                    </HorizontalField>
+                    <HorizontalField label="BPM">
+                      <input
+                        type="number"
+                        min={MIN_BPM}
+                        max={MAX_BPM}
+                        className="input"
+                        value={bpm}
+                        onChange={this.handleChangeBpm}
+                      />
+                    </HorizontalField>
+                    <HorizontalField label="Click" customClass="click-control">
+                      <input
+                        type="checkbox"
+                        id="beatClick"
+                        name="beatClick"
+                        className="switch is-rounded is-info is-medium"
+                        checked={beatClick}
+                        onChange={this.handleToggleClick}
+                      />
+                      <label htmlFor="beatClick" />
+                    </HorizontalField>
+                  </div>
+                </div>
+                <SoundControl
+                  instrument={instrument}
+                  time={time}
+                  bpm={bpm}
+                  volume={volume}
+                  beatClick={beatClick}
+                  parsedText={parsedText}
+                  isPlaying={isPlaying}
+                  onChangePlaying={this.handleChangePlaying}
+                />
+                <div className="volume-control">
+                  <span className="icon">
+                    <i className="fa fa-volume-off" />
+                  </span>
+                  <Slider
+                    min={MIN_VOLUME}
+                    max={MAX_VOLUME}
+                    isFullwidth
+                    value={volume}
+                    onChange={this.handleChangeVolume}
                   />
-                </HorizontalField>
-                <HorizontalField label="Click" customClass="click-control">
-                  <input
-                    type="checkbox"
-                    id="beatClick"
-                    name="beatClick"
-                    className="switch is-rounded is-info is-medium"
-                    checked={beatClick}
-                    onChange={this.handleToggleClick}
-                  />
-                  <label htmlFor="beatClick" />
-                </HorizontalField>
+                  <span className="icon">
+                    <i className="fa fa-volume-up" />
+                  </span>
+                </div>
               </div>
-            </div>
-            <SoundControl
-              instrument={instrument}
-              time={time}
-              bpm={bpm}
-              volume={volume}
-              beatClick={beatClick}
-              parsedText={parsedText}
-              isPlaying={isPlaying}
-              onChangePlaying={this.handleChangePlaying}
-            />
-            <div className="volume-control">
-              <span className="icon">
-                <i className="fa fa-volume-off" />
-              </span>
-              <Slider
-                min={MIN_VOLUME}
-                max={MAX_VOLUME}
-                isFullwidth
-                value={volume}
-                onChange={this.handleChangeVolume}
-              />
-              <span className="icon">
-                <i className="fa fa-volume-up" />
-              </span>
             </div>
           </div>
         </div>

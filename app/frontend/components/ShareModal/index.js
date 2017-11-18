29eecdd6cf28b7a956d8b84048005484c93dfe35
kero-uzura <biruge@biruge.com>
2017-11-19 02:38:05 +0900

    add copy button

--- app/frontend/components/ShareModal/index.js
+++ app/frontend/components/ShareModal/index.js
@@ -1,13 +1,19 @@
 import React, { Component } from "react"
 import classNames           from "classnames"
+import { document }         from "../../utils/browser-dependencies"
 import * as utils           from "../../utils"
 
 export default class ShareModal extends Component {
-  hideModal = () => (
-    this.props.handleSetState({ modal: false })
-  )
+  constructor(props) {
+    super(props)
+    this.state = { showCopyButton: !!document.queryCommandSupported("copy") }
+  }
+  hideModal = () => this.props.handleSetState({ modal: false })
+  handleCopy = () => utils.copyToClipboard(utils.sharedUrl(this.props.url))
+
   render() {
     const { url, isActive } = this.props
+    const { showCopyButton } = this.state
     const sharedUrl = utils.sharedUrl(url)
     const modalClass = classNames("modal", { "is-active": isActive })
     return (
@@ -16,9 +22,17 @@ export default class ShareModal extends Component {
         <div className="modal-content">
           <div className="box">
             <h1 className="has-text-centered title is-5">
-              <a href={sharedUrl} target="_blank">
+              <a id="shared-url" href={sharedUrl} target="_blank">
                 {sharedUrl}
               </a>
+              {showCopyButton && (
+                <button className="button is-small is-info" onClick={this.handleCopy}>
+                  <span className="icon">
+                    <i className="fa fa-copy" />
+                  </span>
+                  <span>copy</span>
+                </button>
+              )}
             </h1>
           </div>
         </div>
--- app/frontend/utils/index.js
+++ app/frontend/utils/index.js
@@ -152,3 +152,22 @@ export const barLength = (score) => (
 
 export const protocol = () => (/^https:/.test(br.location.href) ? "https" : "http")
 export const sharedUrl = (url) => `${protocol()}://${br.location.host}/${url}`
+
+export const copyToClipboard = (text) => {
+  const { document } = br
+
+  const temp = document.createElement("div")
+  temp.appendChild(document.createElement("pre")).textContent = text
+
+  const { style } = temp
+  style.position = "fixed"
+  style.left     = "-100%"
+
+  document.body.appendChild(temp)
+  document.getSelection().selectAllChildren(temp)
+
+  const result = document.execCommand("copy")
+
+  document.body.removeChild(temp)
+  return result
+}

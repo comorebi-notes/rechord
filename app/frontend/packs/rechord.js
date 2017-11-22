7881e91eb7fa24f2f16d574a0bf2fb45df1c3031
kero-uzura <wakabayashita@firstserver.co.jp>
2017-11-22 18:51:41 +0900

    remove turbolinks, Good-bye

--- app/frontend/packs/rechord.js
+++ app/frontend/packs/rechord.js
@@ -8,13 +8,8 @@ const renderComponent = (id, component) => () => {
   const element = document.getElementById(id)
   if (element) ReactDOM.render(component, element)
 }
-// const unmountComponent = (id) => () => {
-//   const element = document.getElementById(id)
-//   if (element) ReactDOM.unmountComponentAtNode(element)
-// }
-const setReact = (id, component) => {
-  document.addEventListener("turbolinks:load", renderComponent(id, component))
-  // document.addEventListener("turbolinks:before-cache", unmountComponent(id))
-}
+const setReact = (id, component) => (
+  document.addEventListener("DOMContentLoaded", renderComponent(id, component))
+)
 
 setReact("rechord", <Rechord />)

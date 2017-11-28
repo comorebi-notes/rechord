d88a5efde6e02afcb9e56f7f9fb976d3e57347a5
kero-uzura <biruge@biruge.com>
2017-11-29 01:11:08 +0900

    card layouts (wip)

--- app/frontend/components/ScoreCard/index.js
+++ app/frontend/components/ScoreCard/index.js
@@ -7,16 +7,22 @@ import * as utils               from "../../utils"
 export default class ScoreCard extends PureComponent {
   render() {
     const { score: { title, token, status, created_at }, isOwn } = this.props
+    const isClosed = status === "closed"
     const showScorePath = `/${token}`
     const editScorePath = `/${token}/edit`
     return (
-      <div className={classNames("box", { closed: status === "closed" })}>
-        <Link to={showScorePath}>
+      <Link to={showScorePath} className="score-card">
+        <div className={classNames("box", { closed: isClosed })}>
           <article className="media">
             <div className="media-content">
               <div className="content">
                 <h3 className="score-title">
                   {title}
+                  {isClosed && (
+                    <span className="icon is-small">
+                      <i className="fa fa-lock" />
+                    </span>
+                  )}
                 </h3>
                 <p className="score-attributes">
                   <time className="created-at">
@@ -26,18 +32,18 @@ export default class ScoreCard extends PureComponent {
               </div>
             </div>
           </article>
-        </Link>
-        {isOwn && (
-          <nav className="field is-grouped">
-            <div className="control">
-              <LinkButton to={editScorePath} icon="pencil-square-o" />
-            </div>
-            <div className="control">
-              <LinkButton to={editScorePath} icon="trash" />
-            </div>
-          </nav>
-        )}
-      </div>
+          {false && isOwn && (
+            <nav className="field is-grouped">
+              <div className="control">
+                <LinkButton to={editScorePath} icon="pencil-square-o" />
+              </div>
+              <div className="control">
+                <LinkButton to={editScorePath} icon="trash" />
+              </div>
+            </nav>
+          )}
+        </div>
+      </Link>
     )
   }
 }

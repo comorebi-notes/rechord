af2a51536a13341dc2ae189a299bc73600bcb8a2
kero-uzura <biruge@biruge.com>
2017-12-09 11:57:33 +0900

    refactor

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,17 +1,17 @@
-rails_root = File.expand_path('../../', __FILE__)
-# rails_env = ENV['RAILS_ENV'] || "development"
+rails_root = File.expand_path("../../", __FILE__)
+# rails_env = ENV["RAILS_ENV"] || "development"
 
-ENV['BUNDLE_GEMFILE'] = rails_root + "/Gemfile"
+ENV["BUNDLE_GEMFILE"] = "#{rails_root}/Gemfile"
 
 worker_processes 2
 working_directory rails_root
 timeout 30
 preload_app true
 
-stderr_path File.expand_path(rails_root + '/log/unicorn_stderr.log', __FILE__)
-stdout_path File.expand_path(rails_root + '/log/unicorn_stdout.log', __FILE__)
+stderr_path File.expand_path("#{rails_root}/log/unicorn_stderr.log", __FILE__)
+stdout_path File.expand_path("#{rails_root}/log/unicorn_stdout.log", __FILE__)
 
-pid File.expand_path(rails_root + '../../tmp/pids/unicorn.pid', __FILE__)
+pid File.expand_path("#{rails_root}/tmp/pids/unicorn.pid", __FILE__)
 
 before_fork do |server, worker|
   defined?(ActiveRecord::Base) and

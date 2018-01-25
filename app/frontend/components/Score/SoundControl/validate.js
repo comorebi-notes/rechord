6ee152a306ada87e4b8319d366424d5dbd67c923
kero-uzura <biruge@biruge.com>
2018-01-26 01:36:13 +0900

    add validate

---
+++ app/frontend/components/Score/SoundControl/validate.js
@@ -0,0 +1,22 @@
+import { document }                        from "../../../utils/browser-dependencies"
+import { addErrorClass, removeErrorClass } from "../../../decorators/scoreEditorDecorator"
+
+export const validate = (score) => {
+  let hasError = false
+
+  const noteElements = document.getElementById("score-editor").getElementsByClassName("root")
+
+  score.forEach((scoreItem, index) => {
+    const { notes } = scoreItem
+    const element   = noteElements[index]
+    if (!notes) {
+      hasError = true
+      addErrorClass(element)
+    } else {
+      removeErrorClass(element)
+    }
+  })
+  return !hasError
+}
+
+export default validate

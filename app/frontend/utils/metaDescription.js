0bf249c34d916f95eb84b805601117e067bd5233
comorebi-notes <wakabayashita@firstserver.co.jp>
2018-06-29 14:36:10 +0900

    remove new line

--- app/frontend/utils/metaDescription.js
+++ app/frontend/utils/metaDescription.js
@@ -1,12 +1,12 @@
 import { document } from "./browser-dependencies"
 
-const MAX_DESCRIPTION_LENGTH = 120
+const MAX_DESCRIPTION_LENGTH = 300
 const ORIGINAL_DESCRIPTION   = document.querySelector('[name="description"]').content
 
 export const set = (text) => {
   const metatag = document.querySelector('[name="description"]')
   if (text && text.length > 0) {
-    metatag.content = text.slice(0, MAX_DESCRIPTION_LENGTH)
+    metatag.content = text.replace(/\n/g, "|").slice(0, MAX_DESCRIPTION_LENGTH)
   } else {
     metatag.content = ORIGINAL_DESCRIPTION
   }

da4e37b673762bfa6fdb03059ace796b699d2ae9
kero <kero@comore.bi>
2018-12-22 10:08:43 +0900

    deploy.db を修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -17,10 +17,10 @@ set :puma_workers,            0
 set :pty,                     true
 set :use_sudo,                false
 set :puma_bind,               "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
-set :puma_state,              '#{shared_path}/tmp/pids/puma.state'
-set :puma_pid,                '#{shared_path}/tmp/pids/puma.pid'
-set :puma_access_log,         '#{release_path}/log/puma.error.log'
-set :puma_error_log,          '#{release_path}/log/puma.access.log'
+set :puma_state,              "#{shared_path}/tmp/pids/puma.state"
+set :puma_pid,                "#{shared_path}/tmp/pids/puma.pid"
+set :puma_access_log,         "#{release_path}/log/puma.error.log"
+set :puma_error_log,          "#{release_path}/log/puma.access.log"
 set :puma_preload_app,        true
 set :puma_worker_timeout,     nil
 set :puma_init_active_record, true

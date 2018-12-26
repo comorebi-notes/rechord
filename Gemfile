4ef170871654d00bf062bc2edefc30b4afa7cd0e
kero <kero@comore.bi>
2018-12-26 10:27:56 +0900

    ruby の version を最新化

--- Gemfile
+++ Gemfile
@@ -5,7 +5,7 @@ git_source(:github) do |repo_name|
   "https://github.com/#{repo_name}.git"
 end
 
-ruby '2.5.3'
+ruby '2.6.0'
 
 # Core
 gem 'rails', '~> 5.2.0'
--- config/deploy.rb
+++ config/deploy.rb
@@ -25,7 +25,7 @@ set :puma_error_log,          "#{release_path}/log/puma.access.log"
 set :puma_preload_app,        true
 set :puma_worker_timeout,     nil
 set :puma_init_active_record, true
-set :rbenv_ruby, '2.5.3'
+set :rbenv_ruby, '2.6.0'
 set :linked_dirs, fetch(:linked_dirs, []).push(
   'log',
   'tmp/pids',

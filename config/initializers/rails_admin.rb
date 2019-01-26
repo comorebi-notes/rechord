f189df4f94b955cc2a0cb2c2c4abcc2dc4b07914
kero <kero@comore.bi>
2019-01-26 20:39:19 +0900

    rails_admin のフォーマットを修正

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -2,15 +2,11 @@ module RailsAdmin
   module Config
     module Fields
       module Types
-        class Datetime < RailsAdmin::Config::Fields::Base
-          register_instance_option :date_format do
-            :default
-          end
+        class Datetime
+          @format = :custom
         end
-        class Date < RailsAdmin::Config::Fields::Types::Datetime
-          register_instance_option :date_format do
-            :default
-          end
+        class Time
+          @format = :custom
         end
       end
     end

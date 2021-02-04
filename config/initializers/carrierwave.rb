96ba8272803fad2b94feff0af87c1e2c0c1fdab0
kero <kero@comore.bi>
2021-02-05 00:53:08 +0900

    ConoHa への接続情報を修正

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,7 +4,7 @@ CarrierWave.configure do |config|
     openstack_tenant:   ENV["CONOHA_TENANT_NAME"],
     openstack_username: ENV["CONOHA_USERNAME"],
     openstack_api_key:  ENV["CONOHA_API_PASSWORD"],
-    openstack_auth_url: ENV["CONOHA_API_AUTH_URL"] + "/tokens"
+    openstack_auth_url: ENV["CONOHA_API_AUTH_URL"]
   }
   config.fog_directory = ENV["CONOHA_CONTAINER_NAME"]
   config.asset_host    = ENV["CONOHA_ASSET_HOST"] + "/" + ENV["CONOHA_CONTAINER_NAME"]

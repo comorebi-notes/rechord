81472b65eb566231f5a8e59d70e08ef3e5de740d
kero <kero@comore.bi>
2024-10-07 01:19:40 +0900

    session_store から redis を削除

--- config/initializers/session_store.rb
+++
@@ -1,6 +0,0 @@
-Rechord::Application.config.session_store :redis_store,
-                                          servers: ENV['REDIS_URL'],
-                                          expire_after: 20.years,
-                                          key: "_#{Rails.application.class.module_parent_name.downcase}_session",
-                                          threadsafe: false,
-                                          ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }
